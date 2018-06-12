7b411175a298d046078ddb992f1e5bd761c8274c
Matsuko Friedland <info@matsuko.ca>
2018-06-11 23:57:24 -0700

    Start add new task functionality

--- comp-2913-react-and-modern-javascript/3_assignment_todo-app/src/App.js
+++ comp-2913-react-and-modern-javascript/3_assignment_todo-app/src/App.js
@@ -4,23 +4,48 @@ import './App.css';
 // Components
 import Task from './components/Task';
 
-const tasks = ['Check this item to remove it from your todo list', 'Item 2', 'Item 3'];
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tasks: ['Check this item to remove it from your todo list', 'Item 2', 'Item 3'],
+      value: '',
+    };
+
+    // Bind custom methods
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // Custom methods
+  handleChange = function(event) {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+
+  handleSubmit = function(event) {
+    if(this.state.value) {
+      this.state.tasks.push(this.state.value);
+    }
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
-        <h1 class="AppTitle">Todo List</h1>
+        <h1 className="AppTitle">Todo List</h1>
         <ul className="AppList">
           {
-            tasks.map((item, index) =>
-              <Task value={ item }/>
+            this.state.tasks.map((item, index) =>
+              <Task value={ item } key={ item } />
             )
           }
         </ul>
 
-        <form className="AppForm">
-          <input type="text" className="AppFormInput" />
+        <form className="AppForm" onSubmit={ this.handleSubmit }>
+          <input type="text" className="AppFormInput" value={ this.state.value } onChange={ this.handleChange } />
           <input type="submit" value="Add" className="AppFormSubmit" />
         </form>
       </div>
