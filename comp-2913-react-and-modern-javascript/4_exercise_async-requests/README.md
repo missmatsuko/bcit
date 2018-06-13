85ba93428c4d7cc151cac76fe129a92276eb7b5e
Matsuko Friedland <info@matsuko.ca>
2018-06-12 19:36:03 -0700

    Display list of countries

--- comp-2913-react-and-modern-javascript/4_exercise_async-requests/README.md
+++ comp-2913-react-and-modern-javascript/4_exercise_async-requests/README.md
@@ -3,11 +3,11 @@
 ## Exercise
 Use a public API to request a list of all countries, and display them in a React App.
 
-- [ ] Create a new application called countries-list.
-- [ ] Install your HTTP Request library of choice. I recommend axios.
-- [ ] To fetch a list of countries in JSON format, make the following request:
+- [ ] Create a new application called countries-list. _Didn't call it this._
+- [ ] Install your HTTP Request library of choice. I recommend axios. _Didn't install library, used fetch_
+- [x] To fetch a list of countries in JSON format, make the following request:
 GET https://restcountries.eu/rest/v2/all
 - [ ] Display a loading spinner while the request is being made.
-- [ ] Display the list of countries.
-- [ ] Each country you list must also display their capital city and continent.
+- [x] Display the list of countries.
+- [x] Each country you list must also display their capital city and continent.
 - [ ] Using the REST Countries API as reference, add the ability to search for countries in your app. You can use a text input and a button to fire the search.
--- comp-2913-react-and-modern-javascript/4_exercise_async-requests/src/App.js
+++ comp-2913-react-and-modern-javascript/4_exercise_async-requests/src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const ENDPOINT = 'https://restcountries.eu/rest/v2/all';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: [],
+    }
+
+    // Bind custom methods
+    // this.pause = this.pause.bind(this);
+  }
+
+
+  // Lifecycle methods
+  componentDidMount() {
+    const self = this;
+
+    fetch(ENDPOINT)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      self.setState({
+        results: json,
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {
+            this.state.results.map((country) =>
+              <li key={ country.alpha3Code }>
+                { country.name }
+                <ul>
+                  <li>{ country.capital }</li>
+                  <li>{ country.region }</li>
+                </ul>
+              </li>
+            )
+          }
+        </ul>
       </div>
     );
   }
