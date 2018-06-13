8d45906962f1469c6e9722b0a270307022654c4a
Matsuko Friedland <info@matsuko.ca>
2018-06-12 20:21:10 -0700

    Get new results on input change

--- comp-2913-react-and-modern-javascript/4_exercise_async-requests/README.md
+++ comp-2913-react-and-modern-javascript/4_exercise_async-requests/README.md
@@ -10,4 +10,4 @@ GET https://restcountries.eu/rest/v2/all
 - [x] Display a loading spinner while the request is being made.
 - [x] Display the list of countries.
 - [x] Each country you list must also display their capital city and continent.
-- [ ] Using the REST Countries API as reference, add the ability to search for countries in your app. You can use a text input and a button to fire the search.
+- [x] Using the REST Countries API as reference, add the ability to search for countries in your app. You can use a text input and a button to fire the search.
--- comp-2913-react-and-modern-javascript/4_exercise_async-requests/src/App.js
+++ comp-2913-react-and-modern-javascript/4_exercise_async-requests/src/App.js
@@ -1,44 +1,76 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const ENDPOINT = 'https://restcountries.eu/rest/v2/all';
+const ENDPOINT_ALL = 'https://restcountries.eu/rest/v2/all';
+const ENDPOINT_NAME = 'https://restcountries.eu/rest/v2/name/';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      loaded: false,
+      query: "",
       results: [],
     }
 
     // Bind custom methods
-    // this.pause = this.pause.bind(this);
+    this.fetch = this.fetch.bind(this);
+    this.search = this.search.bind(this);
   }
 
-
-  // Lifecycle methods
-  componentDidMount() {
+  // Custom methods
+  fetch(endpoint) {
     const self = this;
 
-    fetch(ENDPOINT)
+    fetch(endpoint)
     .then(function(response) {
       return response.json();
     })
+    .catch(function() {
+      console.error("No response");
+      return [];
+    })
     .then(function(json) {
       self.setState({
+        loaded: true,
         results: json,
       });
     });
   }
 
+  search(event) {
+    const value = event.target.value;
+    this.fetch(`${ ENDPOINT_NAME }${ value }`);
+    this.setState({
+      query: value,
+    });
+  }
+
+  // Lifecycle methods
+  componentDidMount() {
+    this.fetch(ENDPOINT_ALL);
+  }
+
   render() {
     return (
       <div className="App">
-        <h1 class="AppTitle">Countries List</h1>
+        <h1 className="AppTitle">Countries List</h1>
         {
-          this.state.results.length
+          // Show input if loaded, else show spinner
+          this.state.loaded
           ?
-          <ul class="AppList">
+          <input className="AppInput" type="text" value={ this.state.query } onChange={ this.search } placeholder="Search" />
+          :
+          <div className="AppSpinner">
+            <span className="screen-reader-text">Loading...</span>
+          </div>
+        }
+        {
+          // Show country list if there are results, else show no results message
+          this.state.loaded && this.state.results.length > 0
+          &&
+          <ul className="AppList">
             {
               this.state.results.map((country) =>
                 <li key={ country.alpha3Code }>
@@ -51,10 +83,10 @@ class App extends Component {
               )
             }
           </ul>
-          :
-          <div className="AppSpinner">
-            <span class="screen-reader-text">Loading...</span>
-          </div>
+        }
+        {
+          this.state.loaded && !this.state.results.length &&
+          <p>No Results</p>
         }
       </div>
     );
