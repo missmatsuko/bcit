3eba470f76091ccfe1448aa8639c5809573017f5
Matsuko Friedland <info@matsuko.ca>
2018-06-19 20:26:41 -0700

    Add basic markup for weather app

--- comp-2913-react-and-modern-javascript/6_assignment_weather-app/src/App.js
+++ comp-2913-react-and-modern-javascript/6_assignment_weather-app/src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <h1>Weather App</h1>
+
+        <h2>3-day Forecast for [Location]</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Monday</th>
+              <th>Tuesday</th>
+              <th>Wednesday</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                [image]
+                [temp]
+              </td>
+              <td>
+                [image]
+                [temp]
+              </td>
+              <td>
+                [image]
+                [temp]
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <h2>Location</h2>
+        <select>
+          <option>Victoria</option>
+          <option>Vancouver</option>
+          <option>Edmonton</option>
+          <option>Toronto</option>
+          <option>Montréal</option>
+        </select>
+
       </div>
     );
   }
