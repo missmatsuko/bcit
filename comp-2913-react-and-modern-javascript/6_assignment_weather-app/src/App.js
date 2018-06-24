81d1b5d796b676f4ea288bf598bd8f61d661042a
Matsuko Friedland <info@matsuko.ca>
2018-06-24 11:15:06 -0700

    Fetch forecast on user input and ugly display all

--- comp-2913-react-and-modern-javascript/6_assignment_weather-app/src/App.js
+++ comp-2913-react-and-modern-javascript/6_assignment_weather-app/src/App.js
@@ -1,14 +1,77 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// Components
+
+const ENDPOINT = 'https://query.yahooapis.com/v1/public/yql';
+const WEATHER_IMAGE = 'https://s.yimg.com/zz/combo?a/i/us/nws/weather/gr/28d.png';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: "vancouver, bc",
+      forecast: [],
+    };
+
+    // Bind custom methods
+    this.getForecast = this.getForecast.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+  }
+
+  // Custom methods
+  getForecast = function() {
+    const query = `select item.forecast from weather.forecast where woeid in (select woeid from geo.places(1) where text='${ this.state.location }') and u='c' limit 3`;
+
+    fetch(`${ ENDPOINT }?format=json&q= ${ query }`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonResponse) => {
+      this.setState({
+        forecast: jsonResponse.query.results.channel.map(channel => {
+          channel.item.forecast.key = Math.random().toString();
+          return channel.item.forecast;
+        }),
+      });
+    });
+  }
+
+  handleLocationChange = function(event) {
+    const newLocation = event.target.value;
+
+    // Update location in state
+    this.setState({
+      location: newLocation,
+    });
+
+    this.getForecast();
+  }
+
+  // Lifecycle methods
   render() {
+    const {forecast, location} = this.state;
+
     return (
       <div className="App">
 
         <h1>Weather App</h1>
 
-        <h2>3-day Forecast for [Location]</h2>
+        <h2>3-day Forecast for {location}</h2>
+        {
+          forecast.map((item, index) =>
+            <div key={item.key}>
+              {item.code}
+              {item.date}
+              {item.day}
+              {item.high}
+              {item.low}
+              {item.day}
+              {item.text}
+            </div>
+          )
+        }
         <table>
           <thead>
             <tr>
@@ -36,12 +99,12 @@ class App extends Component {
         </table>
 
         <h2>Location</h2>
-        <select>
-          <option>Victoria</option>
-          <option>Vancouver</option>
-          <option>Edmonton</option>
-          <option>Toronto</option>
-          <option>Montréal</option>
+        <select onChange={this.handleLocationChange}>
+          <option value="victoria, bc">Victoria</option>
+          <option value="vancouver, bc">Vancouver</option>
+          <option value="edmonton, ab">Edmonton</option>
+          <option value="toronto, on">Toronto</option>
+          <option value="montreal, qc">Montréal</option>
         </select>
 
       </div>
